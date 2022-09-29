f273eb5fa45d7227d8997b60fbbb0d9245ccb85f
matheus1296 <matheuspferreira96@gmail.com>
2022-09-29 15:37:47 -0300

    refactor: class to pass the test

--- src/business/use-cases/CreateCustomer.use-case.ts
+++ src/business/use-cases/CreateCustomer.use-case.ts
@@ -1,1 +1,19 @@
-export class CreateCustomerUseCase {}
+import { Customer } from '../domains/Customer.domains';
+import * as uuid from 'uuid';
+
+export class CreateCustomerUseCase {
+  static generateNewCustomer({
+    document,
+    name,
+  }: {
+    document: number;
+    name: string;
+  }): Customer {
+    const id = uuid.v4();
+    return new Customer({
+      id,
+      name,
+      document,
+    });
+  }
+}
