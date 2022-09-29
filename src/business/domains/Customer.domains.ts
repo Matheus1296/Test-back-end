6d35bd7fc82718ef1f4a06ce3086848ccd30a46f
matheus1296 <matheuspferreira96@gmail.com>
2022-09-29 14:17:53 -0300

    refactor: class to pass the test

--- src/business/domains/Customer.domains.ts
+++ src/business/domains/Customer.domains.ts
@@ -1,7 +1,13 @@
 import { IsNumber, IsString, IsUUID } from 'class-validator';
 import { Entity } from './Entity';
+import { plainToClass } from 'class-transformer';
 
-export class Customer extends Entity<CustomerProps> {}
+export class Customer extends Entity<CustomerProps> {
+  constructor(props: CustomerProps) {
+    const customeProps = plainToClass(CustomerProps, props);
+    super(customeProps);
+  }
+}
 
 export class CustomerProps {
   @IsUUID()
