699640823d5ac31e18abe8bf4e561a7e210067dc
matheus1296 <matheuspferreira96@gmail.com>
2022-09-29 14:34:56 -0300

    feat: one more test case to give an error if the name is empty

--- src/business/domains/Customer.domain.spec.ts
+++ src/business/domains/Customer.domain.spec.ts
@@ -1,7 +1,7 @@
 import { Customer, CustomerProps } from './Customer.domains';
 import * as uuid from 'uuid';
 
-describe('Disciplina', () => {
+describe('Customer', () => {
   let customer: Customer;
   const props: CustomerProps = {
     document: 123456789,
@@ -17,5 +17,21 @@ describe('Disciplina', () => {
   it('should validate that a course has all the correct parameters.', () => {
     expect(customer.validate().length).toBe(0);
     expect(customer.getValue()).toEqual(props);
+  });
+
+  describe('.name', () => {
+    it('should give an error if it has no name.', () => {
+      customer.setField('name', undefined as unknown as string);
+
+      const validationErrors = customer.validate();
+      expect(validationErrors).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            property: 'name',
+          }),
+        ]),
+      );
+      expect(() => customer.getValue()).toThrow();
+    });
   });
 });
