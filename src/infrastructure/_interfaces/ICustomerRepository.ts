de19e2c8ece52c692f35d6dc1dc9820a0a41c481
matheus1296 <matheuspferreira96@gmail.com>
2022-09-29 16:11:41 -0300

    refactor: add token to not cause type error

--- src/infrastructure/_interfaces/ICustomerRepository.ts
+++ src/infrastructure/_interfaces/ICustomerRepository.ts
@@ -1,5 +1,7 @@
 import { Customer } from '../../business/domains/Customer.domains';
 
+export const ICustomerRepositoryToken = Symbol('ICustomerRepositoryToken');
+
 export interface ICustomerRepository {
   create(custumer: Customer): Promise<Customer>;
 }
