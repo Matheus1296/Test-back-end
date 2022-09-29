21d2f6c3b46673a2b4e9df1918a413363d1d33c1
matheus1296 <matheuspferreira96@gmail.com>
2022-09-29 13:27:11 -0300

    feat: create class for validation

---
+++ src/business/domains/Entity.ts
@@ -0,0 +1,42 @@
+import { validateSync, ValidationError } from 'class-validator';
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export abstract class Entity<P extends Object> {
+  protected readonly props: P;
+
+  protected constructor(props: P) {
+    const validationErrors = validateSync(props);
+    if (validationErrors.length) {
+      throw validationErrors;
+    }
+    this.props = props;
+  }
+
+  validate(): ValidationError[] {
+    return validateSync(this.props);
+  }
+
+  getValue(options?: { skipValidation: boolean }): P {
+    const validationErrors: ValidationError[] = options?.skipValidation
+      ? []
+      : this.validate();
+    if (!validationErrors.length) {
+      return this.props;
+    } else {
+      throw validationErrors;
+    }
+  }
+
+  setField<F extends keyof P>(field: F, newValue: P[F]): void {
+    if (!this.props || this.props.hasOwnProperty(field)) {
+      this.props[field] = newValue;
+      this.validate();
+    } else {
+      throw new Error(`Property ${field} is protected and cannot be changed.`);
+    }
+  }
+
+  valueOf(): P {
+    return this.getValue();
+  }
+}
