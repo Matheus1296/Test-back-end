1cf533710c99d050289afd3bfc67a03d8a4a65bb
matheus1296 <matheuspferreira96@gmail.com>
2022-09-29 16:12:23 -0300

    refactor: class to pass the test

--- src/business/use-cases/CreateCustomer.use-case.ts
+++ src/business/use-cases/CreateCustomer.use-case.ts
@@ -1,6 +1,30 @@
 import { Customer } from '../domains/Customer.domains';
+import {
+  ICustomerRepository,
+  ICustomerRepositoryToken,
+} from '../../infrastructure/_interfaces/ICustomerRepository';
+import { Inject, Injectable } from '@nestjs/common';
 
+@Injectable()
 export class CreateCustomerUseCase {
+  constructor(
+    @Inject(ICustomerRepositoryToken)
+    private readonly customerRepository: ICustomerRepository,
+  ) {}
+
+  async run(request: {
+    document: number;
+    name: string;
+    id: string;
+  }): Promise<Customer> {
+    const customer = new Customer({
+      id: request.id,
+      document: request.document,
+      name: request.name,
+    });
+    return await this.customerRepository.create(customer);
+  }
+
   static generateNewCustomer({
     id,
     document,
