79f3a092c2a49031b4747b53d26736b85121da1b
matheus1296 <matheuspferreira96@gmail.com>
2022-09-29 17:12:50 -0300

    feat: test to update in one of the parameters

--- src/business/use-cases/UpdateCustomer.use-case.spec.ts
+++ src/business/use-cases/UpdateCustomer.use-case.spec.ts
@@ -36,4 +36,35 @@ describe('UpdateCustomerUseCase', () => {
     expect(customer).toEqual(customerMockUpdate);
     expect(customerRepository.findOne).toHaveBeenCalledWith(id);
   });
+  it('should update a customer without one of the parameters ', async () => {
+    const id = uuid.v4();
+    const request: { id: string; document?: number; name?: string } = {
+      name: 'test test test',
+      id,
+    };
+
+    const customerMock = new Customer({
+      id,
+      document: 987654321,
+      name: 'test',
+    });
+
+    const customerMockUpdate = new Customer({
+      document: 987654321,
+      name: 'test test test',
+      id,
+    });
+
+    const customerRepository: ICustomerRepository = {
+      create: jest.fn(),
+      findOne: jest.fn().mockImplementation(() => customerMock),
+      updateOne: jest.fn().mockImplementation(() => customerMockUpdate),
+    };
+
+    const updateCustomerUseCase = new UpdateCustomerUseCase(customerRepository);
+    const customer = await updateCustomerUseCase.run(request);
+
+    expect(customer).toEqual(customerMockUpdate);
+    expect(customerRepository.findOne).toHaveBeenCalledWith(id);
+  });
 });
