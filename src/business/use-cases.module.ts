f177b02fd37fb863d58921d0558b9c636d2a3326
matheus1296 <matheuspferreira96@gmail.com>
2022-09-29 22:19:10 -0300

    feat: import from redis module

--- src/business/use-cases.module.ts
+++ src/business/use-cases.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { CreateCustomerUseCase } from './use-cases/CreateCustomer.use-case';
 import { FindCustomerUseCase } from './use-cases/FindCustomer.use-case';
 import { UpdateCustomerUseCase } from './use-cases/UpdateCustomer.use-case';
+import { RedisRepositoriesModule } from '../infrastructure/Redis/redis-repositories.module';
 
 @Module({
+  imports: [RedisRepositoriesModule],
   providers: [
     CreateCustomerUseCase,
     FindCustomerUseCase,
