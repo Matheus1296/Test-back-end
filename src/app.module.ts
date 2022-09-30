2da9bcac8947ff76c55e84f2acd22944e07f9af4
matheus1296 <matheuspferreira96@gmail.com>
2022-09-29 22:16:09 -0300

    feat: configuration for using redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { RedisModule } from '@nestjs-modules/ioredis';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { CustomerController } from './infrastructure/REST/customer.controller';
+import { UseCasesModule } from './business/use-cases.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '../.env',
+      isGlobal: true,
+    }),
+    RedisModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        config: {
+          url: config.get<string>('REDIS_URL'),
+          password: config.get<string>('REDIS_PASSWORD'),
+        },
+      }),
+    }),
+    UseCasesModule,
+  ],
+  controllers: [CustomerController],
 })
 export class AppModule {}
